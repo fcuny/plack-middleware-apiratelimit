98d2a0a7f5d9f72521d80ab6ae5d8cb2a16e66c8
franck cuny <franck@lumberjaph.net>
2010-02-28 17:13:48 +0100

    initial commit

---
+++ lib/Plack/Middleware/APIRateLimit.pm
@@ -0,0 +1,33 @@
+package Plack::Middleware::APIRateLimit;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Plack::Middleware::APIRateLimit -
+
+=head1 SYNOPSIS
+
+  use Plack::Middleware::APIRateLimit;
+
+=head1 DESCRIPTION
+
+Plack::Middleware::APIRateLimit is
+
+=head1 AUTHOR
+
+franck cuny E<lt>franck.cuny@rtgi.frE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
